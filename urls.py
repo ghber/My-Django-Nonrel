d0d923c73a2f36998e974189e4e3e0507225a02e
Alex Clark <aclark@aclark.net>
2011-06-10 14:29:58 -0400

    Revert

--- urls.py
+++ urls.py
@@ -1,16 +1,9 @@
 from django.conf.urls.defaults import *
-from django.contrib.auth.forms import AuthenticationForm
 
 handler500 = 'djangotoolbox.errorviews.server_error'
 
 urlpatterns = patterns('',
-    (r'^$', 'django.views.generic.simple.redirect_to', {'url': '/guestbook/', }),
-    (r'^guestbook/', include('guestbook.urls')),
-
-    (r'^accounts/create_user/$', 'guestbook.views.create_new_user'),
-    (r'^accounts/login/$', 'django.contrib.auth.views.login',
-        {'authentication_form': AuthenticationForm,
-        'template_name': 'guestbook/login.html',}),
-    (r'^accounts/logout/$', 'django.contrib.auth.views.logout',
-        {'next_page': '/guestbook/',}),
+    ('^_ah/warmup$', 'djangoappengine.views.warmup'),
+    ('^$', 'django.views.generic.simple.direct_to_template',
+     {'template': 'home.html'}),
 )
